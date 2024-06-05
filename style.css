d4d20acf091342a2a2db1b54e4db31f2ee04a5bb
navya-sri1 <velagalanavyasri@gmail.com>
2024-06-05 11:51:52 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,178 @@
+body{
+    margin:0;
+    background-color: black;
+    color:white;
+    background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR84-Y0zmoY5mU2seeQZy5vxWt2hIX86BInpA&s");
+
+}
+
+.navbar-right
+{
+    display:flex;
+    flex-direction:row;
+    text-align: center;
+    padding-left:500px;
+   
+}
+.anchor
+{
+    padding: 20px;
+    text-decoration:underline;
+    color:greenyellow;
+    font-size: 25px;
+}
+.home{
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    height:60vh;
+    align-items: center;
+    
+}
+.home-left
+{
+    width:70vw;
+
+}
+.home-m{
+    text-align: center;
+    color:aqua;
+    font-size: 40px;
+    padding-top: 50px;
+
+}
+.home-right{
+    width:30vw;
+    align-items:flex-end;
+    align-content: end;
+    
+}
+.logo{
+    height: 300px;
+    width: 300px;
+   
+}
+.home1{
+    color:yellow;
+    padding-left: 200px;
+}
+.home2{
+    color:orchid;
+    font-size: 30px;
+    padding-left: 350px;
+}
+.about
+{ 
+text-align: center;
+    
+}
+.about-top{
+    color:orange;
+    font-size:40px;
+    text-decoration:underline;
+    letter-spacing: 1px;
+
+}
+  .about-bottom{
+    font-size:25px;
+    padding-left:350px;
+    padding-right:350px;
+    padding-bottom: 100px;
+    padding-top:20px;
+    display:flex;
+    word-spacing: 6px;
+  }
+.skills
+{
+    display:flex;
+    flex-direction :row;
+    justify-content: space-between;
+    padding-top:1px;
+    padding-bottom:100px;
+}
+.skills-left{
+    font-size: 20px;
+    width:30vw;
+    text-align: center;
+    color:rgb(155, 255, 127);
+    padding-top:200px;
+    text-decoration: underline;
+}
+.skills-right{
+    width:70vw;
+   
+}
+.heading{
+    font-size: 30px;
+    color:orange;
+    letter-spacing: 1px;
+}
+.theo{
+    font-size:20px;
+    padding:50px;
+    padding-top:60px;
+}
+.skills1{
+    text-align: center;
+    color:lightpink
+}
+.education{
+    text-align: center;
+}
+.edu-top{
+    color:powderblue;
+    font-size:40px;
+    text-decoration:underline;
+    letter-spacing: 1px;
+
+}
+.services{
+    display:flex;
+    flex-direction:row;
+    padding:50px;
+    padding-top: 20px;
+    justify-content:space-evenly;
+    align-items: center;
+
+}
+.services-card{
+    border:2px green solid;
+    padding:10px;
+    width:300px;
+    color:bisque
+
+}
+.edu{
+    color:azure;
+    font-size: 20px;
+    padding:2px;
+}
+.project{
+    text-align: center;
+}
+.project-top{
+    color:yellow;
+    font-size:40px;
+    text-decoration:underline ;
+    letter-spacing:1px;
+    
+}
+.project-info{
+    font-size:25px;
+    padding-left: 600px;
+   text-align: center;
+}
+.contact
+{
+    text-align: center;
+}
+.contact-info{
+    font-size:25px;
+}
+.social-icon-img{
+    height: 50px;
+    width:50px;
+}
+.social-icons{
+    padding: 30px;;
+}
